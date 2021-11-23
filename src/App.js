30b6bd3111a6cfde75a0be96466fba4d923357f4
Aviad Mizrachi <aviadm@gmail.com>
2021-11-23 09:01:24 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useAuth } from '@frontegg/react'
+import {useHistory} from "react-router-dom";
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+  const history = useHistory();
+  // const { user, isAuthenticated } = useAuth(); // Use this to avoid redirect to /account/login
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+        {isAuthenticated ? (
+          <div>
+            <div>
+              <img src={user?.profilePictureUrl} alt={user?.name}/>
+            </div>
+            <div>
+              <span>Logged in as: {user?.name}</span>
+            </div>
+            <div>
+              <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+            </div>
+            <div>
+              <button onClick={() => history.push('/account/logout')}>Click me to logout</button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h1>Please login</h1>
+            <button onClick={() => {
+              history.push('/account/login');
+            }}>Login</button>
+          </div>
+        )}
+
+      </div>
   );
 }
 
